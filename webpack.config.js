d6eb34798fb357ca7a5d3a84c78392d734e253b3
jaimemendozadev <jaimemendozadev@gmail.com>
2019-01-08 15:57:58 -0800

    Add webpack bundle analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -3,25 +3,31 @@ const webpack = require("webpack")
 
 const { HotModuleReplacementPlugin } = webpack
 const Dotenv = require("dotenv-webpack")
+const webpackMerge = require("webpack-merge")
 
 const publicFolder = path.resolve(__dirname, "public")
 const entry = path.resolve(__dirname, "dev/index.js")
+const presets = require("./webpack/presets/webpack.analyze")
 
-module.exports = ({ mode } = { mode: "production" }) => ({
-  entry,
-  mode,
-  output: {
-    path: publicFolder,
-    filename: "bundle.js",
-  },
-  devServer: {
-    contentBase: "public",
-    historyApiFallback: true, // Handles Client Side Routing
-  },
-  module: {
-    rules: [
-      { test: /\.(js|jsx)$/, exclude: /node_modules/, use: "babel-loader" },
-    ],
-  },
-  plugins: [new Dotenv(), new HotModuleReplacementPlugin()],
-})
+module.exports = ({ mode } = { mode: "production" }) =>
+  webpackMerge(
+    {
+      entry,
+      mode,
+      output: {
+        path: publicFolder,
+        filename: "bundle.js",
+      },
+      devServer: {
+        contentBase: "public",
+        historyApiFallback: true, // Handles Client Side Routing
+      },
+      module: {
+        rules: [
+          { test: /\.(js|jsx)$/, exclude: /node_modules/, use: "babel-loader" },
+        ],
+      },
+      plugins: [new Dotenv(), new HotModuleReplacementPlugin()],
+    },
+    presets(),
+  )
